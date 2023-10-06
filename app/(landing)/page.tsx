83176a74539961c108b91140034f3791e6cad695
Majid Al-Raimi <majid@42.solutions>
2023-10-06 23:29:22 +0300

    Added Auth 💪

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="w-full h-screen flex items-center justify-center ">
+      السلام عليكم
+    </main>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,22 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  theme: {},
+  plugins: [require('daisyui')],
+  // create custom font
+  daisyui: {
+    themes: [
+      {
+        ad3oni: {
+          "primary": '#2b0d69',
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
       },
-    },
-  },
-  plugins: [],
+      "light",
+    ]
+  }
 }
 export default config
