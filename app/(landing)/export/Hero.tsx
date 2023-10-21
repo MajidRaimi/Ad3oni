61fe73baef78df48dd6af35a395ed4e8f8b7af18
Majid Al-Raimi <majid@42.solutions>
2023-10-21 20:45:43 +0300

    Hero Screen Fixed

--- app/(landing)/export/Hero.tsx
+++ app/(landing)/export/Hero.tsx
@@ -3,7 +3,7 @@ import { NavBar, PrayerFormattedText, ShareWithUsButton } from "../components"
 
 const Hero = () => {
     return (
-        <section className='h-screen background flex flex-col  justify-between text-white'>
+        <section className='min-h-screen background flex flex-col  justify-between text-white'>
             <NavBar />
             <PrayerFormattedText />
             <ShareWithUsButton />
